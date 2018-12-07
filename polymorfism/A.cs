5abb3cfe55a3f5a0c99531216cd7419b6b538479
Elliott Lie <Elliott.DTK.Lie@lystkom.se>
2018-12-07 12:39:10 +0100

    Add project files.

---
+++ polymorfism/A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorfism
+{
+    class A
+    {
+        public string Description()
+        {
+            return "The object is of type A";
+        }
+    }
+
+    class B : A
+    {
+        public new string Description()
+        {
+            return "Objectet är av typen B";
+        }
+    }
+}
---
+++ polymorfism/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polymorfism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void skapa_Click(object sender, EventArgs e)
+        {
+            A Obj;
+            if (radioButton1.Checked)
+            {
+                Obj = new A();
+            }
+            else
+            {
+                Obj = new B();
+            }
+
+            if (Obj is B)
+            {
+                textBox1.Text = (Obj as B).Description();
+            }
+
+            else
+            {
+                textBox1.Text = Obj.Description();
+
+            }
+
+        }
+    }
+}
